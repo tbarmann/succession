7350921f807b1c7d2885af3bd37a6732728a87bd
Katrina Owen <katrina.owen@gmail.com>
2016-02-12 19:13:51 +1000

    Introduce indirection with case statement
    The HEREDOC makes it very difficult to change a small
    bit of the text at a time. Now each verse (or even each line)
    can be tackled separately.

--- song.rb
+++ song.rb
@@ -1,56 +1,63 @@
 class Song
   def lyrics
-    <<-SONG
-I know an old lady who swallowed a fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
+    (1..8).map {|i| verse(i)}.join("\n")
+  end
 
-I know an old lady who swallowed a spider.
-It wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
+  private
 
-I know an old lady who swallowed a bird.
-How absurd to swallow a bird!
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a cat.
-Imagine that, to swallow a cat!
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a dog.
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a goat.
-Just opened her throat and swallowed a goat!
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a cow.
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat.
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-
-I know an old lady who swallowed a horse.
-She's dead, of course!
-    SONG
+  def verse(i)
+    case i
+    when 1
+      "I know an old lady who swallowed a fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    when 2
+      "I know an old lady who swallowed a spider.\n" +
+      "It wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    when 3
+      "I know an old lady who swallowed a bird.\n" +
+      "How absurd to swallow a bird!\n" +
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    when 4
+      "I know an old lady who swallowed a cat.\n" +
+      "Imagine that, to swallow a cat!\n" +
+      "She swallowed the cat to catch the bird.\n" +
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    when 5
+      "I know an old lady who swallowed a dog.\n" +
+      "What a hog, to swallow a dog!\n" +
+      "She swallowed the dog to catch the cat.\n" +
+      "She swallowed the cat to catch the bird.\n" +
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    when 6
+      "I know an old lady who swallowed a goat.\n" +
+      "Just opened her throat and swallowed a goat!\n" +
+      "She swallowed the goat to catch the dog.\n" +
+      "She swallowed the dog to catch the cat.\n" +
+      "She swallowed the cat to catch the bird.\n" +
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    when 7
+      "I know an old lady who swallowed a cow.\n" +
+      "I don't know how she swallowed a cow!\n" +
+      "She swallowed the cow to catch the goat.\n" +
+      "She swallowed the goat to catch the dog.\n" +
+      "She swallowed the dog to catch the cat.\n" +
+      "She swallowed the cat to catch the bird.\n" +
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    when 8
+      "I know an old lady who swallowed a horse.\n" +
+      "She's dead, of course!\n"
+    end
   end
 end
