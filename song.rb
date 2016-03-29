3b4b9011a04c53f5e9ecc29814cc859efbda39b0
Katrina Owen <katrina.owen@gmail.com>
2016-03-28 19:40:52 -0600

    Introduce :aside abstraction
    Use it for the second line of each verse. This makes that line identical, and we can ignore it.

--- song.rb
+++ song.rb
@@ -1,4 +1,4 @@
-class Critter < Struct.new(:name, :qualifier)
+class Critter < Struct.new(:name, :qualifier, :aside)
   def epiteth
     [name, qualifier].compact.join(" ")
   end
@@ -6,14 +6,18 @@ end
 
 class Song
   DATA = [
-    ["horse"],
-    ["cow"],
-    ["goat"],
-    ["dog"],
-    ["cat"],
-    ["bird"],
-    ["spider", "that wriggled and jiggled and tickled inside her"],
-    ["fly"],
+    ["horse", nil, "She's dead, of course!"],
+    ["cow", nil, "I don't know how she swallowed a cow!"],
+    ["goat", nil, "Just opened her throat and swallowed a goat!"],
+    ["dog", nil, "What a hog, to swallow a dog!"],
+    ["cat", nil, "Imagine that, to swallow a cat!"],
+    ["bird", nil, "How absurd to swallow a bird!"],
+    [
+      "spider",
+      "that wriggled and jiggled and tickled inside her",
+      "It wriggled and jiggled and tickled inside her.",
+    ],
+    ["fly", nil, "I don't know why she swallowed the fly. Perhaps she'll die."],
   ]
 
   attr_reader :critters
@@ -31,40 +35,40 @@ class Song
     case i
     when 1
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % critters.last(i).first.aside
     when 2
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "It wriggled and jiggled and tickled inside her." +
+      "%s\n" % critters.last(i).first.aside +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 3
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "How absurd to swallow a bird!" +
+      "%s\n" % critters.last(i).first.aside +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 4
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "Imagine that, to swallow a cat!" +
+      "%s\n" % critters.last(i).first.aside +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 5
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "What a hog, to swallow a dog!" +
+      "%s\n" % critters.last(i).first.aside +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 6
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "Just opened her throat and swallowed a goat!" +
+      "%s\n" % critters.last(i).first.aside +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 7
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "I don't know how she swallowed a cow!" +
+      "%s\n" % critters.last(i).first.aside +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 8
       "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
-      "%s\n" % "She's dead, of course!"
+      "%s\n" % critters.last(i).first.aside
     end
   end
 
