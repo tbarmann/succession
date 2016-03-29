e5a6e72ddd332578c3651de32f3cfbf08be61544
Katrina Owen <katrina.owen@gmail.com>
2016-03-28 19:35:33 -0600

    Pass i to chain instead of hard-coded numbers.
    This makes one tiny difference between different cases identical.
    We can ignore it.

--- song.rb
+++ song.rb
@@ -35,32 +35,32 @@ class Song
     when 2
       "I know an old lady who swallowed a %s.\n" % "spider" +
       "%s\n" % "It wriggled and jiggled and tickled inside her." +
-      "%s\n" % chain(2) +
+      "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 3
       "I know an old lady who swallowed a %s.\n" % "bird" +
       "%s\n" % "How absurd to swallow a bird!" +
-      "%s\n" % chain(3) +
+      "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 4
       "I know an old lady who swallowed a %s.\n" % "cat"+
       "%s\n" % "Imagine that, to swallow a cat!" +
-      "%s\n" % chain(4) +
+      "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 5
       "I know an old lady who swallowed a %s.\n" % "dog" +
       "%s\n" % "What a hog, to swallow a dog!" +
-      "%s\n" % chain(5) +
+      "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 6
       "I know an old lady who swallowed a %s.\n" % "goat" +
       "%s\n" % "Just opened her throat and swallowed a goat!" +
-      "%s\n" % chain(6) +
+      "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 7
       "I know an old lady who swallowed a %s.\n" % "cow" +
       "%s\n" % "I don't know how she swallowed a cow!" +
-      "%s\n" % chain(7) +
+      "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 8
       "I know an old lady who swallowed a %s.\n" % "horse" +
