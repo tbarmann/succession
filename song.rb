4d169e73b33823f7f26a4d3c1534a733a4c1c6d5
Katrina Owen <katrina.owen@gmail.com>
2016-03-28 19:34:27 -0600

    Remove extra space in string formatting
    It no longer serves a purpose.

--- song.rb
+++ song.rb
@@ -31,40 +31,40 @@ class Song
     case i
     when 1
       "I know an old lady who swallowed a %s.\n" % "fly" +
-      "%s\n"                                     % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 2
       "I know an old lady who swallowed a %s.\n" % "spider" +
-      "%s\n"                                     % "It wriggled and jiggled and tickled inside her." +
-      "%s\n"                                     % chain(2) +
-      "%s\n"                                     % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % "It wriggled and jiggled and tickled inside her." +
+      "%s\n" % chain(2) +
+      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 3
       "I know an old lady who swallowed a %s.\n" % "bird" +
-      "%s\n"                                     % "How absurd to swallow a bird!" +
-      "%s\n"                                     % chain(3) +
-      "%s\n"                                     % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % "How absurd to swallow a bird!" +
+      "%s\n" % chain(3) +
+      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 4
       "I know an old lady who swallowed a %s.\n" % "cat"+
-      "%s\n"                                     % "Imagine that, to swallow a cat!" +
-      "%s\n"                                     % chain(4) +
-      "%s\n"                                     % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % "Imagine that, to swallow a cat!" +
+      "%s\n" % chain(4) +
+      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 5
       "I know an old lady who swallowed a %s.\n" % "dog" +
-      "%s\n"                                     % "What a hog, to swallow a dog!" +
-      "%s\n"                                     % chain(5) +
-      "%s\n"                                     % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % "What a hog, to swallow a dog!" +
+      "%s\n" % chain(5) +
+      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 6
       "I know an old lady who swallowed a %s.\n" % "goat" +
-      "%s\n"                                     % "Just opened her throat and swallowed a goat!" +
-      "%s\n"                                     % chain(6) +
-      "%s\n"                                     % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % "Just opened her throat and swallowed a goat!" +
+      "%s\n" % chain(6) +
+      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 7
       "I know an old lady who swallowed a %s.\n" % "cow" +
-      "%s\n"                                     % "I don't know how she swallowed a cow!" +
-      "%s\n"                                     % chain(7) +
-      "%s\n"                                     % "I don't know why she swallowed the fly. Perhaps she'll die."
+      "%s\n" % "I don't know how she swallowed a cow!" +
+      "%s\n" % chain(7) +
+      "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 8
       "I know an old lady who swallowed a %s.\n" % "horse" +
-      "%s\n"                                     % "She's dead, of course!"
+      "%s\n" % "She's dead, of course!"
     end
   end
 
