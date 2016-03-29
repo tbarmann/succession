95d12bd9acfc3c3dde0a402f0ef3aa7843363f88
Katrina Owen <katrina.owen@gmail.com>
2016-03-28 19:37:27 -0600

    Use the first critter's :name in each verse
    The first line of the verse was hard-coding an abstraction
    we had already defined. This removes the difference, and
    we can ignore it.

--- song.rb
+++ song.rb
@@ -30,40 +30,40 @@ class Song
   def verse(i)
     case i
     when 1
-      "I know an old lady who swallowed a %s.\n" % "fly" +
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 2
-      "I know an old lady who swallowed a %s.\n" % "spider" +
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "It wriggled and jiggled and tickled inside her." +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 3
-      "I know an old lady who swallowed a %s.\n" % "bird" +
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "How absurd to swallow a bird!" +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 4
-      "I know an old lady who swallowed a %s.\n" % "cat"+
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "Imagine that, to swallow a cat!" +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 5
-      "I know an old lady who swallowed a %s.\n" % "dog" +
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "What a hog, to swallow a dog!" +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 6
-      "I know an old lady who swallowed a %s.\n" % "goat" +
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "Just opened her throat and swallowed a goat!" +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 7
-      "I know an old lady who swallowed a %s.\n" % "cow" +
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "I don't know how she swallowed a cow!" +
       "%s\n" % chain(i) +
       "%s\n" % "I don't know why she swallowed the fly. Perhaps she'll die."
     when 8
-      "I know an old lady who swallowed a %s.\n" % "horse" +
+      "I know an old lady who swallowed a %s.\n" % critters.last(i).first.name +
       "%s\n" % "She's dead, of course!"
     end
   end
